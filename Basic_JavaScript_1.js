68336a6cb098efd813216893ad053da24c48f8d7
ScDavis27 <64049331+ScDavis27@users.noreply.github.com>
2020-04-23 18:42:46 -0400

    Delete "Basic_JavaScript_1.js"
    Delete "Basic_JavaScript_1.js"

--- Basic_JavaScript_1.js
+++
@@ -1,7 +0,0 @@
-alert("Hello, World! Welcome to my test page!")
-function My_First_Function() {
-		var string="Kiss me, I'm Irish!";
-		var result=string.fontcolor("green");
-		document.getElementById("Irish").innerHTML=
-		result;
-	}
